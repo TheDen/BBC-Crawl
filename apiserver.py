787d2d89489419d53afb34122a5ba58787462909
Patchy <pnx-bot@previousnext.com.au>
2017-02-07 01:31:29 +1100

    add err handling

--- apiserver.py
+++ apiserver.py
@@ -1,58 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import flask
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Resource
 import json
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 import os
+import sys
 import ssl
-
+from flask import render_template
+# grab creds from the env var
 MONGODB_URL = os.environ.get('MONGODB_URL')
-client = MongoClient(MONGODB_URL,ssl_ca_certs="./sslfile.cert")
+# load client with ssl certificate
+try:
+  client = MongoClient(MONGODB_URL,ssl_ca_certs="./sslfile.cert")
+except:
+  print "err: can't connect to db. Make sure MONGODB_URL is set"
+  sys.exit()
+
+# check db collecion, load it into memory
 db = client.admin
 collection = db.bbcdata
+try:
+  data = list(collection.find())
+except:
+  print "err: can't find collection on db"
+  sys.exit()
+
 app = Flask(__name__)
 
-data = list(collection.find())
-
-from flask import render_template
-
+# handle 404s
 @app.errorhandler(404)
 def page_not_found(e):
-    return flask.make_response("page not founditry. Try /api/v1/?query=yourquery", 404)
+  return flask.jsonify(error=404, text=str(e)), 404
 
 @app.route("/")
-def hello():
-    return "hit /api/v1/?query=yourquery"
+def base_url():
+    return flask.make_response("/api/v1/articles/?query=example", 200)
 
-@app.route('/api/v1/')
-def show_user_profile():
+@app.route('/api/v1/articles/')
+def get_matches():
    query = request.args.get('query')
-
    if not query:
-    return flask.make_response("need nonempty query parameter", 200)
-   
+     return flask.jsonify(error=404, text="empty query"), 404
+
    objectid = []
    j = 0
+   # look for can insensitive search text in headline and article
    for i in data:
     if query.lower() in i.values()[0].lower() or query.lower() in i.values()[3].lower():
-     print j
      objectid.append(j)
     j += 1
 
    if len(objectid) == 0:
-     return flask.make_response("no match", 200)
+     return flask.jsonify(error=404, text="no match"), 200
    resp = []
    for i in objectid:
-    match = data
-    resp.append(json.dumps({'url': match[i].values()[1] , 'headline': match[i].values()[0], 'article': match[i].values()[3] }))
-    
+    resp.append(json.dumps({'url': data[i].values()[1] , 'headline': data[i].values()[0], 'article': data[i].values()[3] }))
+
+   # fix string to make it valid json
    response_str = json.dumps(resp, sort_keys=False, separators=(',', ':')).replace('["','[').replace('"]',']').replace("\\\"", "\"").replace("\\\\\"", "\\\"").replace("}\"", "}").replace("\"{", "{")
    response = flask.make_response(response_str, 200)
    response.headers['Content-Type'] = 'application/json'
    return response
-  
+
 if __name__ == "__main__":
     app.run()
--- spider.py
+++ spider.py
@@ -1,71 +1,86 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-
 import scrapy
 import requests
-import html2text
 import re
 from urlparse import urlparse
 import json
-from newspaper import Article
 from goose import Goose
-
-g = Goose()
-# for dispatcher
-from scrapy.xlib.pydispatch import dispatcher
 from scrapy import signals
+import argparse
+import sys
 
 class bbcspider(scrapy.Spider):
  name = "spider"
  allowed_domains = ["bbc.com"]
- start_urls = [
-      'http://www.bbc.com',
-      'http://www.bbc.com/news/',
-      'http://www.bbc.com/news/world',
-      'http://www.bbc.com/news/world/asia',
-      'http://www.bbc.com/sport/',
-      'http://www.bbc.com/travel/',
-      'http://www.bbc.com/capital/',
-      'http://www.bbc.com/culture/',
-      'http://www.bbc.com/future/',
-      'http://www.bbc.com/earth/uk',
-        ]
+ def __init__(self,*args, **kwargs):
+   super(bbcspider, self).__init__(*args, **kwargs)
+   start_urls = []
+   urlargs = kwargs.get('start_urls')
+   if not urlargs:
+     #default urls
+     self.start_urls = [
+         'http://www.bbc.com',
+         'http://www.bbc.com/news/',
+         'http://www.bbc.com/news/world',
+         'http://www.bbc.com/news/world/asia',
+         'http://www.bbc.com/sport/',
+         'http://www.bbc.com/travel/',
+         'http://www.bbc.com/capital/',
+         'http://www.bbc.com/culture/',
+         'http://www.bbc.com/future/',
+         'http://www.bbc.com/earth/uk',
+         ]
+   else:
+     self.start_urls = kwargs.get('start_urls').split(',')
 
- def __init__(self):
-   dispatcher.connect(self.spider_closed, signals.spider_closed)
-
- def spider_closed(self, spider):
-  print "done"
-  with open('output.json') as data_file:
-    data = json.load(data_file)
+ def closed(self, response):
+  # grab the --output arg
+  try:
+    outputfile =  filter(re.compile("--output=").match, sys.argv)[0].split("--output=")[1]
+  except:
+   print "err: can't open file"
+   return
+  if not outputfile:
+    print "err: output file empty"
+    return
+  with open(outputfile) as data_file:
+    try:
+      data = json.load(data_file)
+    except:
+      print "err: can't parse" + outputfile
+      return
     uniq = { each['url'] : each for each in data }.values()
-  with open('output.json', 'w+') as out:
-    json.dump(uniq, out)
+  with open(outputfile, 'w+') as out:
+    try:
+      json.dump(uniq, out)
+    except:
+      print "err: can't write file"
 
  def parse(self, response):
   hrefs = response.css('::attr(href)').extract()
   paths = []
   for i in range(0, len(hrefs)):
    paths.append(urlparse(hrefs[i]).path)
+  # remove duplicates
   urls = []
   for i in paths:
    if i not in urls:
     urls.append(i)
-  for i in range(0, len(urls)): 
+  for i in range(0, len(urls)):
      yield scrapy.Request(response.urljoin(urls[i]),
           callback=self.urlparse)
 
  def urlparse(self, response):
-
+  # bbc.com articles have a 8 digits in the url
   match = re.search('[0-9]{8}',response.url)
   if match:
-
     url = response.url
-    g = Goose()
-    article = g.extract(url=url)
+    article = Goose().extract(url=url)
 
+    # JSON output data
     yield {
      'headline': article.title,
      'url': url,
      'text': article.cleaned_text,
-        } 
+        }
